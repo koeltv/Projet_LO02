bd1ca9f75989a5a2ab1cb9d6eb7c6070b0be830e
Valentin Koeltgen <47000714+koeltv@users.noreply.github.com>
2021-09-24 14:44:54 +0200

    First run of Java files generation with Modelio

---
+++ src/Player.java
@@ -0,0 +1,15 @@
+
+public class Player {
+    private int score;
+
+    public int getScore() {
+        // Automatically generated method. Please delete this comment before entering specific code.
+        return this.score;
+    }
+
+    public void setScore(final int value) {
+        // Automatically generated method. Please delete this comment before entering specific code.
+        this.score = value;
+    }
+
+}
