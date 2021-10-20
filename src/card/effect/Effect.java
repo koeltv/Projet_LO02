b2c019fff11a52c92d710c40defe0a172b79e9ee
Valentin Koeltgen <47000714+koeltv@users.noreply.github.com>
2021-10-20 18:29:54 +0200

    Updated Effect java doc

--- src/card/effect/Effect.java
+++ src/card/effect/Effect.java
@@ -3,9 +3,25 @@ package card.effect;
 import card.CardName;
 import player.Player;
 
+/**
+ * The interface Effect.
+ */
 public interface Effect {
+    /**
+     * Apply effect.
+     *
+     * @param cardUser the card user
+     * @param target   the target
+     * @return whether the effect was successfully applied or not
+     */
     boolean applyEffect(Player cardUser, Player target);
 
+    /**
+     * Choose target player.
+     *
+     * @param cardName the card name
+     * @return the chosen player
+     */
     Player chooseTarget(CardName cardName);
 
 }
