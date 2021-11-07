7a8bcee9688a915b3f65aa242153b39aaa7447f7
Valentin Koeltgen <47000714+koeltv@users.noreply.github.com>
2021-11-07 18:54:12 +0100

    Updated Javadoc

--- src/com/controller/GameController.java
+++ src/com/controller/GameController.java
@@ -29,9 +29,21 @@ public class GameController {
 
     private final ActiveView view;
 
+    /**
+     * Action choice at the end of a game.
+     */
     enum GameAction {
+        /**
+         * Restart a new game with existing players.
+         */
         RESTART_GAME,
+        /**
+         * Restart a new game with new players.
+         */
         RESET_GAME,
+        /**
+         * End the program.
+         */
         STOP
     }
 
